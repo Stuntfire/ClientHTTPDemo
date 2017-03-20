05f531b7a5a658b413ff497122c55798bae219c3
Søren Roed Schack <soer622t@edu.easj.dk>
2017-03-20 22:07:07 +0100

    Opgave 4: Kalder hotel nr. 3 for at ændre hotellets navn.

--- ClientHTTPDemo/Program.cs
+++ ClientHTTPDemo/Program.cs
@@ -27,11 +27,10 @@ namespace ClientHTTPDemo
                 try
                 {
                     HttpResponseMessage response = client.GetAsync(urlStringHotel3).Result;
-                    if (response.IsSuccessStatusCode)
                     {
                         var hotel3 = response.Content.ReadAsAsync<Hotel>().Result;
 
-                        Console.WriteLine($"OPGAVE 1: \n\tHotel_No: {hotel3.Hotel_No} \n\tHotelname: { hotel3.Name} \n\tAdresse: {hotel3.Address} ");
+                        Console.WriteLine($"OPGAVE 1: \n\tHotel No.: \t{hotel3.Hotel_No} \n\tHotelname: \t{ hotel3.Name} \n\tAdresse: \t{hotel3.Address} ");
                     }
                 }
                 catch (Exception e)
@@ -41,37 +40,41 @@ namespace ClientHTTPDemo
 
 
                 //Opgave 2
-                //string urlStringGetAllHotels = "api/hotels";
-
-                //try
-                //{
-                //    HttpResponseMessage responsGetAllHotels = client.GetAsync(urlStringGetAllHotels).Result;
-                //    if (responsGetAllHotels.IsSuccessStatusCode)
-                //    {
-                //        var GetAllHotels = responsGetAllHotels.Content.ReadAsAsync<IEnumerable<Hotel>>().Result;
-
-                //        var RoskildeList = from h in GetAllHotels
-                //                           where h.Address.Contains("Roskilde")
-                //                           select h;
-
-                //        foreach (var item in RoskildeList)
-                //        {
-                //            Console.WriteLine($"\nOPGAVE 2: \n\tHotel_No: {item.Hotel_No} \n\tHotelname: { item.Name} \n\tAdresse: {item.Address} ");
-                //        }
-                //    }
-                //}
-                //catch (Exception e)
-                //{
-                //    Console.WriteLine("Der er sket en fejl : " + e.Message);
-                //}
-
-                //Opgave 3
-
                 string urlStringGetAllHotels = "api/hotels";
-                string urlStringGetAllRooms = "api/rooms";
+
                 try
                 {
                     HttpResponseMessage responsGetAllHotels = client.GetAsync(urlStringGetAllHotels).Result;
+                    if (responsGetAllHotels.IsSuccessStatusCode)
+                    {
+                        var GetAllHotels = responsGetAllHotels.Content.ReadAsAsync<IEnumerable<Hotel>>().Result;
+
+                        var RoskildeList = from h in GetAllHotels
+                                           where h.Address.Contains("Roskilde")
+                                           select h;
+
+                        //foreach (var item in RoskildeList)
+                        //{
+                        //    Console.WriteLine(item);
+                        //}
+                        foreach (var item in RoskildeList)
+                        {
+                            Console.WriteLine($"\nOPGAVE 2: \n\tHotel No.: \t{ item.Hotel_No } \n\tHotelname: \t{ item.Name } \n\tAdresse: \t{ item.Address } ");
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Der er sket en fejl : " + e.Message);
+                }
+
+                //Opgave 3
+
+                string urlStringGetAllHotels2 = "api/hotels";
+                string urlStringGetAllRooms = "api/rooms";
+                try
+                {
+                    HttpResponseMessage responsGetAllHotels = client.GetAsync(urlStringGetAllHotels2).Result;
                     if (responsGetAllHotels.IsSuccessStatusCode)
                     {
                         var GetAllHotels = responsGetAllHotels.Content.ReadAsAsync<IEnumerable<Hotel>>().Result;
@@ -109,7 +112,7 @@ namespace ClientHTTPDemo
 
                                 foreach (var item in RoskildeHotelsAndRooms)
                                 {
-                                    Console.WriteLine(item);
+                                    Console.WriteLine($"\nOPGAVE 3: \n\tHotel No.: \t{ item.Hotel_No } \n\tHotelname: \t{ item.Name } \n\tAdresse: \t{ item.Address } \n\tRoom No.: \t{ item.Room_No } \n\tRoomtype: \t{ item.Price }");
                                 }
                             }
                         }
@@ -125,9 +128,44 @@ namespace ClientHTTPDemo
                     Console.WriteLine("Der er sket en fejl : " + e.Message);
                 }
 
+                //Opgave 4
+                Console.WriteLine("\nOPGAVE 4");
+                string urlStringGetHotel3 = "api/hotels/3";
 
+                try
+                {
+                    HttpResponseMessage getResponse = client.GetAsync(urlStringGetHotel3).Result;
+                    {
+                        var hotel3Response = getResponse.Content.ReadAsAsync<Hotel>().Result;
 
+                        var MyUpdatedHotel = new Hotel()
+                        {
+                            Hotel_No = 3,
+                            Address = "Inexpensive Road 7 3333 Cheaptown",
+                            Name = "Discount"
+                        };
+
+                        MyUpdatedHotel.Name = "Osvald";
+
+                        var response = client.PutAsJsonAsync<Hotel>("API/Hotels/3", MyUpdatedHotel).Result;
+                        if (response.IsSuccessStatusCode)
+                        {
+                            Console.WriteLine("Du har opdateret et hotel");
+                            Console.WriteLine("Statuskode : " + response.StatusCode);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Fejl, hotellet blev ikke opdateret");
+                            Console.WriteLine("Statuskode : " + response.StatusCode);
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Der er sket en fejl : " + e.Message);
+                }
             }
+
+        }
         }
     }
-}
